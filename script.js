073dcef3c6dea97422154a1eaef635b6d7205c96
Rohit Roy <rohitroy2003.rr@gmail.com>
2024-09-15 17:51:57 +0530

    done with leetmetrics app

--- script.js
+++ script.js
@@ -26,25 +26,85 @@ document.addEventListener("DOMContentLoaded", () => {
      }
 
      async function fetchUserDetails(username){
-          //    video continue from :- 42:07 / 1:21:45
-          const url = `https://leetcode-stats-api.herokuapp.com/users/api/${username}`;   
-          try{
+
+        try{
             searchButton.textContent = "Loading...";
             searchButton.disabled = true;
-            const response = await fetch(url);
+            statsContainer.style.display = "none";
+
+
+            const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+            const targetUrl = 'https://leetcode.com/graphql/';
+
+            const myHeaders = new Headers();
+            myHeaders.append("content-type", "application/json");
+
+           const graphql = JSON.stringify({
+           query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+           variables: { "username": `${username}` }
+            })
+          const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: graphql,
+          };
+
+           const response = await fetch(proxyUrl +  targetUrl, requestOptions);
             if(!response.ok){
                 throw new Error("Unable to fetch the User details");
             }
-            const data = await response.json();
-            console.log("Loggin data:", data);
+            const parseddata = await response.json();
+            console.log("Loggin data:", parseddata);
+
+             displayuserdata(parseddata);
+
           }
           catch(error){
-             statsContainer.innerHTML=`<p>No data found</p>`
+             statsContainer.innerHTML=`<p>${error.message}</p>`
           }
           finally{
              searchButton.textContent = "Search";
              searchButton.disabled = false;
           }
+    }
+
+    function updateProgress(solved,total,label,circle){
+            const progressDegree = (solved/total)*100;
+            circle.style.setProperty("--progress-degree",`${progressDegree}%`);
+            label.textContent = `${solved}/${total}`;
+    }
+
+
+    const displayuserdata = function(parseddata){
+          const totalQues = parseddata.data.allQuetionsCount[0].count;
+          const totalEasyQues = parseddata.data.allQuetionsCount[1].count;
+          const totalMediumQues = parseddata.data.allQuetionsCount[2].count;
+          const totalHardQues = parseddata.data.allQuetionsCount[3].count;
+
+         const solvedTotalQues = parseddata.data.matchedUser.submitStats.acSubmissionNum[0].count;
+         const solvedTotalEasyQues = parseddata.data.matchedUser.submitStats.acSubmissionNum[1].count;
+         const solvedTotalMediumQues = parseddata.data.matchedUser.submitStats.acSubmissionNum[2].count;
+         const solvedTotalHardQues = parseddata.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
+         updateProgress(solvedTotalEasyQues,totalEasyQues,easyLabel,easyProgressCircle);
+         updateProgress(solvedTotalMediumQues,totalMediumQues,mediumLabel,mediumProgressCircle);    
+         updateProgress(solvedTotalHardQues,totalHardQues,hardLabel,hardProgressCircle);
+
+         const carddata = [
+              {label: "Overall submissions",value: parseddata.data.matchedUser.submitStats.totalSubmissionNum[0].submissions},
+              {label: "Easy submissions",value: parseddata.data.matchedUser.submitStats.totalSubmissionNum[1].submissions},
+              {label: "Medium submissions",value: parseddata.data.matchedUser.submitStats.totalSubmissionNum[2].submissions},
+              {label: "Hard submissions",value: parseddata.data.matchedUser.submitStats.totalSubmissionNum[3].submissions},  
+         ];
+
+         cardStatsContainer.innerHTML = carddata.map(
+            data=>
+                `<div class="card"> 
+                  <h4>${data.label}</h4>
+                  <p>${data.value}</p>
+                 </div>                                          
+                 `
+         ).join("")
     }
 
     
